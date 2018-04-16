24b75561a4b8354de70d62cef36902fee0dcfc8c
sebknight <37192621+sebknight@users.noreply.github.com>
2018-04-17 11:15:51 +1200

    Add existing file

---
+++ basqueApp/js/script.js
@@ -0,0 +1,49 @@
+// Call this function when the document is ready
+$(document).ready(function () {
+
+//  var items = ["#menu-item_olives"];
+
+// Toggle cart visibility
+  $('#order').click(function () {
+    $('#cart').toggleClass('is-visible');
+
+  });
+
+
+  $('#menu').click(function () {
+      $('#category-1-header').toggleClass('is-open');
+
+  });
+
+  // Add a click listener to the element matching selector #category-1-header
+  $('#category-1-header').click(function () {
+
+    // Toggle the class `is-open` on the element matching selector `#subcategory-1-body
+    $('#subcategory-1-header').toggleClass('is-open');
+  });
+
+  $('#subcategory-1-header').click(function () {
+
+    $('#subcategory-1-body').toggleClass('is-open');
+  });
+
+  $('#subcategory-2-header').click(function () {
+
+    $('#subcategory-2-body').toggleClass('is-open');
+  });
+
+  $('#subcategory-3-header').click(function () {
+
+    $('#subcategory-3-body').toggleClass('is-open');
+  });
+
+// Add to cart example
+//$('#menu-item_olives').click(function () {
+  // Add an object to cart array
+/*  cart.push({
+    name: "Olives",
+    price: "$5.00",
+  });
+}); */
+
+});
